6a3f310eb5788c74a8dad7b160be5067e9d8ee7c
guneet446 <guneet446@gmail.com>
2020-08-27 17:58:09 +0530

    Updated home page

--- saathi/lib/main.dart
+++ saathi/lib/main.dart
@@ -105,6 +105,15 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
 
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //launch for  direct calling
+        },
+        child: Icon(Icons.call),
+        backgroundColor: Colors.green,
+      ),
+
+
       drawer: Drawer(
         // Add a ListView to the drawer. This ensures the user can scroll
         // through the options in the drawer if there isn't enough vertical
