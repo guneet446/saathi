65621951b82cbcd403d630e185ee8286aadbaf12
guneet446 <guneet446@gmail.com>
2020-08-27 17:27:17 +0530

    updated registration form

--- saathi/lib/main.dart
+++ saathi/lib/main.dart
@@ -119,11 +119,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
 
       drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
         child: ListView(
-          // Important: Remove any padding from the ListView.
           padding: EdgeInsets.zero,
           children: <Widget>[
             Container(
